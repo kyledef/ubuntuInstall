9097b8831d6d5827c060c25bc72dbfd7e54b841b
kyledef <kyle.e.defreitas@gmail.com>
2015-10-27 01:08:46 -0400

    installed chrome

--- installDev.sh
+++ installDev.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo apt-get update
 sudo apt-get install -y curl
 
@@ -17,7 +16,13 @@ curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
 
 sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 
-sudo apt-get install -y guake sublime-text-installer oracle-java8-installer nodejs git apache2 apache2-utils mysql-server libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt php5-oauth php5-curl phpmyadmin python-pip python-virtualenv mongodb libc6:i386 libstdc++6:i386 ant lib32z1 lib32z1-dev nautilus-open-terminal python-dev libblas-dev liblapack-dev python-dev libatlas-base-dev gfortran
+sudo apt-get update
+
+sudo apt-get install -y guake sublime-text-installer oracle-java8-installer nodejs git apache2 apache2-utils mysql-server libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt php5-oauth php5-curl phpmyadmin python-pip python-virtualenv mongodb libc6:i386 libstdc++6:i386 ant lib32z1 lib32z1-dev nautilus-open-terminal python-dev libblas-dev liblapack-dev python-dev libatlas-base-dev gfortran gimp mongodb && sudo apt-get dist-upgrade -y
+
+echo "Install Google Chrome Browser for development"
+wget https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb -O google-chrome.deb
+sudo dpkg -i google-chrome.deb
 
 # Configure MYSQL DB
 sudo mysql_install_db
@@ -53,19 +58,22 @@ nodemon -v
 gulp -v
 
 echo "Installing the Android SDK"
-wget http://dl.google.com/android/android-sdk_r24.2-linux.tgz
-tar -xvzf android-sdk_r24.2-linux.tgz
+wget http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz -O android-sdk.tgz
+tar -xvzf android-sdk.tgz
 mkdir ~/tools
 mv android-sdk-linux/ ~/tools/android-sdk
 
 echo "Installing the Android Studio IDE"
-wget https://dl.google.com/dl/android/studio/ide-zips/1.3.1.0/android-studio-ide-141.2135290-linux.zip -O android-studio.zip
+wget https://dl.google.com/dl/android/studio/ide-zips/1.4.0.10/android-studio-ide-141.2288178-linux.zip -O android-studio.zip
 unzip android-studio.zip
 mv android-studio ~/tools/android-studio
 
+echo "Installing Heroku (Without logging in)"
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 
 echo "Installing visual studio code"
-wget http://go.microsoft.com/fwlink/?LinkID=534108 -O VScode.zip #Need to organize better download Link
+wget https://az764295.vo.msecnd.net/public/0.9.1/VSCode-linux64.zip -O VScode.zip #Need to organize better download Link
 unzip VScode.zip
 mv VSCode-linux-x64 ~/tools/visual-studio-code
 
@@ -82,11 +90,10 @@ android -h
 echo "Attempting to pull down android packages"
 android list sdk -u --all --extended
 
-android update sdk --no-ui --filter android-22,android-19,tools,platform-tools,build-tools 
+android update sdk --no-ui --filter android-23,android-22,tools,platform-tools,build-tools-23.0.1
 
 echo "Configure Git for default message"
 git config --global push.default simple
 
 
-echo "Installing Heroku (Without logging in)"
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
